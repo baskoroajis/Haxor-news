7024e1e7dd7f4f4b3ada02f16f8880057fb308df
baskoro aji susetyo <baskoroajisusetyo@gmail.com>
2020-02-07 10:44:19 +0700

    first commit

---
+++ Haxor-news/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Haxor-news
+//
+//  Created by Baskoro on 07/02/20.
+//  Copyright © 2020 Baskoro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List(posts){post in
+            Text(post.newsTitle)
+        }
+       
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+let posts = [
+       post(id: "1",newsTitle: "baskoro",newsDescription: "7 years experience as ios engineer"),
+       post(id: "1",newsTitle: "baskoro",newsDescription: "7 years experience as ios engineer")
+]
+
+struct post:Identifiable{
+    var id: String
+    var newsTitle:String
+    var newsDescription:String
+}
+
